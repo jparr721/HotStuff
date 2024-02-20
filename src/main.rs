f04e55306822ce1401fa0d1837c3408c2b702721
Jarred Parr <jparr721@gmail.com>
2024-02-20 13:11:39 -0500

    init working startup for node

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use anyhow;
 
-pub mod block;
-pub mod crypto;
-pub mod node;
+mod block;
+mod crypto;
+mod node;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    Ok(())
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Debug)
+        .init();
+
+    let mut node = node::Node::new("1".to_owned(), 2000, None).await?;
+    node.run().await
 }
--- src/node.rs
+++ src/node.rs
@@ -1,45 +1,156 @@
+use anyhow::Context;
+use futures::future::join_all;
+use rand::seq::IteratorRandom;
+use std::sync::{Arc, Mutex};
+use std::{collections::HashMap, time::Duration};
+use tokio::io::AsyncReadExt;
+use tokio::net::{TcpListener, TcpStream};
+
+use log::{debug, error, info};
 use serde::{Deserialize, Serialize};
+use tokio_util::codec::{Decoder, Encoder, Framed};
 
 use crate::block::Block;
 
+static MAX_NUM_PEERS: usize = 25;
+static MAX_RETRY_TIMES: usize = 5;
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct HelloMessage {
-    id: u8,
+    id: usize,
     name: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct NewPeersMessage {
+    peers: Vec<String>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum Message {
     Hello(HelloMessage),
+    NewPeers(NewPeersMessage),
 }
 
 /// A Node that is able to participate in consensus.
-#[derive(Debug, Clone)]
-struct Node {
-    // The genesis block.
+#[derive(Debug)]
+pub struct Node {
+    /// The genesis block.
     pub b0: Block,
 
-    // The locked block.
+    /// The locked block.
     pub b_lock: Block,
 
-    // The last executed block.
+    /// The last executed block.
     pub b_exec: Block,
+
+    /// The node ID
+    pub id: String,
+
+    /// The peer IP Port combinations. Thread safe.
+    pub peers: Arc<Mutex<HashMap<String, usize>>>,
+
+    /// The peer subset of connections. Thread safe.
+    pub peer_cxns: Arc<Mutex<Vec<Option<TcpStream>>>>,
+
+    /// The TCP listener for the node
+    pub listener: TcpListener,
 }
 
 impl Node {
-    pub fn new() -> Self {
+    pub async fn new(
+        id: String,
+        port: usize,
+        peers: Option<HashMap<String, usize>>,
+    ) -> anyhow::Result<Self> {
         // TODO change this to be a real block
         // Make the genesis block.
         let b0 = Block::default();
 
-        Self {
+        let ip_addr = format!("0.0.0.0:{}", port);
+        let listener = TcpListener::bind(&ip_addr).await?;
+
+        let peers = Arc::new(Mutex::new(peers.unwrap_or(HashMap::new())));
+
+        info!("Node {} bound on {}", id, ip_addr);
+
+        Ok(Self {
             b_lock: b0.clone(),
             b_exec: b0.clone(),
             b0,
-        }
+            id,
+            peers,
+            peer_cxns: Arc::new(Mutex::new(vec![])),
+            listener,
+        })
     }
 
-    async fn run() {
-        loop {}
+    pub async fn run(&mut self) -> anyhow::Result<()> {
+        let mut rng = rand::thread_rng();
+        // TODO handle node init failures and re-fetch from the map.
+        self.peer_cxns = Arc::new(Mutex::new(
+            join_all(
+                self.peers
+                    .lock()
+                    .unwrap()
+                    .iter()
+                    .choose_multiple(&mut rng, MAX_NUM_PEERS)
+                    .into_iter()
+                    .map(|(ip, port)| async move {
+                        info!("Establishing connection to {}:{}", ip, port);
+                        let mut retries = 0;
+                        loop {
+                            // Try to make the connection.
+                            let stream = TcpStream::connect(format!("{}:{}", ip, port)).await;
+
+                            // If this failed, then sleep for 5 seconds and try again.
+                            if stream.is_err() {
+                                tokio::time::sleep(Duration::from_secs(5)).await;
+                                retries += 1;
+
+                                // If we've exceeded our maximum configured number of retries, then return `None`.
+                                if retries >= MAX_RETRY_TIMES {
+                                    error!("Node {}:{} failed to connect", ip, port);
+                                    // TODO don't let this happen. Instead, try a different IP address and use that. If we've
+                                    // exhausted our pool, log an error and move on.
+                                    return None;
+                                }
+                            } else {
+                                // We can be reasonably sure that this unwrap will go successfully since we've
+                                // already checked the error. If this somehow does not work,
+                                // then we know something terible has happened.
+                                return Some(stream.context(
+                                "Something went horribly wrong acquiring the TCP connection",
+                            ).unwrap());
+                            }
+                        }
+                    })
+                    .collect::<Vec<_>>(),
+            )
+            .await,
+        ));
+
+        loop {
+            match self.listener.accept().await {
+                Ok((mut stream, _)) => {
+                    // Grab a reference to listen for updates
+                    let peer_cxns = self.peer_cxns.clone();
+
+                    tokio::spawn(async move {
+                        let mut buffer = Vec::new();
+                        match stream.read_to_end(&mut buffer).await {
+                            Ok(_) => match bincode::deserialize::<Message>(&buffer) {
+                                Ok(data) => {
+                                    debug!("Got data {:?}", data);
+                                }
+                                Err(e) => error!("Failed to deserialize message; error = {:?}", e),
+                            },
+                            Err(e) => error!("Failed to read data from socket; error = {:?}", e),
+                        };
+                    });
+                }
+                Err(e) => error!("Error accepting socket; error = {:?}", e),
+            }
+        }
     }
 }
