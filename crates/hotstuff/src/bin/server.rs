6f8d3b9b2fa469823379fa0523318659da376ef6
Jarred Parr <jparr721@gmail.com>
2024-02-21 10:28:55 -0500

    - Refactor entire project to use crates - INitialize testing crate - Update version of "ahash" from 0.8.8 to 0.8.9 - Update version of "anyhow" from 1.0.79 to 1.0.80 - Update version of "cc" from 1.0.83 to 1.0.86 - Add new package "clap" with version 4.5.1 - Update version of "hermit-abi" from 0.3.5 to 0.3.6 - Add new package "heck" with version 0.4.1 - Update version of "serde" from 1.0.196 to 1.0.197 - Update version of "serde_derive" from 1.0.196 to 1.0.197

--- crates/hotstuff/src/bin/server.rs
+++ crates/hotstuff/src/bin/server.rs
@@ -1,22 +1,21 @@
 use anyhow;
 use futures::Future;
-use log::info;
-
-mod block;
-mod crypto;
-mod node;
+use log::{info, warn};
+use hotstuff::node;
 
 pub async fn run(shutdown: impl Future) -> anyhow::Result<()> {
     let (shutdown_tx, shutdown_rx) = tokio::sync::mpsc::channel::<()>(1);
     let mut terminate =
         tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate()).unwrap();
-    let mut node = node::Node::new("1".to_owned(), 2000, None, shutdown_rx).await?;
+
+    let id = uuid::Uuid::new_v4().to_string();
+    let mut node = node::Node::new(id, 2000, None, shutdown_rx).await?;
     tokio::select! {
         res = node.run() => {
             res
         },
         _ = terminate.recv() => {
-            info!("terminating");
+            warn!("terminating");
             let _ = shutdown_tx.send(()).await;
             Ok(())
         },
---
+++ crates/hotstuff/src/lib.rs
@@ -0,0 +1,6 @@
+use anyhow;
+use futures::Future;
+
+pub mod block;
+pub mod crypto;
+pub mod node;
